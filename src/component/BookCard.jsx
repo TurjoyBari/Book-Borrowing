9aff6d0adda911b77f8fa0b3ab813187e22dbf64
TurjoyBari <bariturjo@gmail.com>
2026-05-02 04:22:17 +0600

    data img issue

--- src/component/BookCard.jsx
+++ src/component/BookCard.jsx
@@ -1,48 +1,60 @@
-import { Button, Card, Chip, Separator } from '@heroui/react';
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
-import { BiDownload } from 'react-icons/bi';
-import { FaHeart } from 'react-icons/fa';
+import { Button, Card, Chip, Separator } from "@heroui/react";
+import Image from "next/image";
 
-const BookCard = ({book}) => {
-    return (
-        <Card className="border rounded-xl">
-            <div className="relative w-full aspect-square">
-            <Image
-                src={book.imageUrl}
-                fill
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                alt={book.title}
-                className="object-cover rounded-xl"
-                />
+import Link from "next/link";
+import React from "react";
+import { BiDownload } from "react-icons/bi";
+import { FaHeart } from "react-icons/fa";
 
-                <Chip size="sm" className="absolute right-2 top-2">{book.category}</Chip>
-            </div>
+const BookCard = ({ book }) => {
 
-            <div>
-                <h2 className="font-medium">{book.title}</h2>
-            </div>
+  return (
+    <Card className="border rounded-xl">
+      <div className="relative w-full aspect-square">
+        <Image
+          key={book.id}
+          src={book.imageUrl}
+          fill
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          alt={book.title}
+          className="object-cover rounded-xl"
+        />
 
-           <div className="flex gap-5">
-             <div className="flex items-center gap-2">
-                <p><FaHeart /></p>
-                <p>{book.likes}</p>
-            </div>
+        <Chip size="sm" className="absolute right-2 top-2">
+          {book.category}
+        </Chip>
+      </div>
 
-            <Separator orientation="vertical"/>
+      <div>
+        <h2 className="font-medium">{book.title}</h2>
+      </div>
 
+      <div className="flex gap-5">
+        <div className="flex items-center gap-2">
+          <p>
+            <FaHeart />
+          </p>
+          <p>{book.likes}</p>
+        </div>
 
-               <div className="flex items-center gap-2">
-                <p><BiDownload /></p>
-                <p>{book.downloads}</p>
-            </div>
-           </div>
+        <Separator orientation="vertical" />
 
-     <Link href={`/all-photos/${book.id}`}>      <Button variant="outline" className={'w-full'}>View</Button></Link>
-            
-        </Card>
-    );
+        <div className="flex items-center gap-2">
+          <p>
+            <BiDownload />
+          </p>
+          <p>{book.downloads}</p>
+        </div>
+      </div>
+
+      <Link href={`/all-photos/${book.id}`}>
+        {" "}
+        <Button variant="outline" className={"w-full"}>
+          View
+        </Button>
+      </Link>
+    </Card>
+  );
 };
 
 export default BookCard;
