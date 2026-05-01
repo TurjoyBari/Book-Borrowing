85aae649534911c7fd51ec6abac2a4f2f74440fc
TurjoyBari <bariturjo@gmail.com>
2026-05-02 00:27:46 +0600

    fixe data final 1

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactCompiler: true,
-  images: {
-    remotePatterns: [
-      {
-        
-        hostname: 'images.unsplash.com',
-        
-      },
-    ]
-  },
+ 
 
   /* config options here */
 };
